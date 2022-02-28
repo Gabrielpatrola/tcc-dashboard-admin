18006c2e3fa2ea7f9647b614d9ec898e2469c76a
Gabriel Augusto Almeida <gabriel@patrola.tech>
2022-02-28 11:29:14 -0400

    feat: Modify page 404 text

--- src/views/pages/page404/Page404.js
+++ src/views/pages/page404/Page404.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import {
-  CButton,
-  CCol,
-  CContainer,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMagnifyingGlass } from '@coreui/icons'
+import { CButton, CCol, CContainer, CInputGroup, CRow } from '@coreui/react'
+import { useHistory } from 'react-router-dom'
 
 const Page404 = () => {
+  const history = useHistory()
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -19,17 +12,15 @@ const Page404 = () => {
           <CCol md={6}>
             <div className="clearfix">
               <h1 className="float-start display-3 me-4">404</h1>
-              <h4 className="pt-3">Oops! You{"'"}re lost.</h4>
+              <h4 className="pt-3">Oops! Você parece perdido!</h4>
               <p className="text-medium-emphasis float-start">
-                The page you are looking for was not found.
+                A página que você procura não existe!
               </p>
             </div>
             <CInputGroup className="input-prepend">
-              <CInputGroupText>
-                <CIcon icon={cilMagnifyingGlass} />
-              </CInputGroupText>
-              <CFormInput type="text" placeholder="What are you looking for?" />
-              <CButton color="info">Search</CButton>
+              <CButton color="info" onClick={() => history.push('/dashboard')}>
+                Volte para o dashboard
+              </CButton>
             </CInputGroup>
           </CCol>
         </CRow>
